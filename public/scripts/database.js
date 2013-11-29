23cc2aa9f69ad56438643c81ef2b695cf3b3e403
aepp <aleksandr.epp@gmail.com>
2013-11-29 01:01:22 +0100

    upload fixing

--- public/scripts/database.js
+++ public/scripts/database.js
@@ -51,7 +51,7 @@ $(document).ready(function() {
 		                onUploadSuccess: function (file, data, response) {
 		                	logoUploaded = file.name;
 		                	$('#bankLogo').val(logoUploaded);
-		                	alert(response);
+		                	alert(data);
 //		                	alert($('input[name="bankLogo"]').val());
 		                	$('#logoPreview').attr('src', 'uploads/bank-logo/'+logoUploaded);
 
