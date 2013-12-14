2cb1aca49484a867655fc7954350e51bd29f45a5
aepp <aleksandr.epp@gmail.com>
2013-12-14 21:52:07 +0100

    added resizing functionality
    uploaded bank-logos will be resized to 60x115 and saved into
    wp-bank-logos-dir.
    
    first test

--- public/scripts/uploadify/uploadify.php
+++ public/scripts/uploadify/uploadify.php
@@ -28,6 +28,48 @@ if (!empty($_FILES)) {
 			copy($src.$_FILES['Filedata']['name'], $dest.$_FILES['Filedata']['name']);
 		}
 		echo '1';
+		
+		// Create image from file
+		$image = null;
+		switch(strtolower($_FILES['Filedata']['type']))
+		{
+			case 'image/jpeg': case 'image/jpg':
+				$image = imagecreatefromjpeg($_FILES['Filedata']['tmp_name']);
+				break;
+			case 'image/png':
+				$image = imagecreatefrompng($_FILES['Filedata']['tmp_name']);
+				break;
+			case 'image/gif':
+				$image = imagecreatefromgif($_FILES['Filedata']['tmp_name']);
+				break;
+			default:
+				exit('Unsupported type: '.$_FILES['image']['type']);
+		}
+		if($image != null){
+			// Target dimensions
+			$max_width = 115;
+			$max_height = 60;
+			
+			// Get current dimensions
+			$old_width  = imagesx($image);
+			$old_height = imagesy($image);
+			
+			// Calculate the scaling we need to do to fit the image inside our frame
+			$scale = min($max_width/$old_width, $max_height/$old_height);
+			
+			// Get the new dimensions
+			$new_width  = ceil($scale*$old_width);
+			$new_height = ceil($scale*$old_height);
+			
+			// Create new empty image
+			$new = imagecreatetruecolor($new_width, $new_height);
+			
+			// Resize old image into new
+			imagecopyresampled($new, $image,
+						0, 0, 0, 0,
+						$new_width, $new_height, $old_width, $old_height);
+			imagejpeg($new, $dest.$_FILES['Filedata']['name'].'-60x115');
+		}
 	} else {
 		echo 'Invalid file type.';
 	}
